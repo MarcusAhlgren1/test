d873e40d128a1f373ca2377f3fecbeb53eec8fef
Marcus Ahlgren <marcuz_ahlgren@hotmail.com>
2021-02-15 09:00:36 +0100

    init commit

--- src/githubexempel/GitHubExempel.java
+++ src/githubexempel/GitHubExempel.java
@@ -16,7 +16,7 @@ public class GitHubExempel {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Detta är ett Github exempel."),
+        System.out.println("Detta är ett Github exempel.");
         System.out.println("detta är en ny rad");
         
         

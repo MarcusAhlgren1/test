977a3e668106e0ae34739767151e8b44f6872f0c
Marcus Ahlgren <marcuz_ahlgren@hotmail.com>
2021-02-15 08:59:44 +0100

    init commit

---
+++ src/githubexempel/GitHubExempel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubexempel;
+
+/**
+ *
+ * @author haga
+ */
+public class GitHubExempel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Detta är ett Github exempel."),
+        System.out.println("detta är en ny rad");
+        
+        
+        
+    }
+    
+}
